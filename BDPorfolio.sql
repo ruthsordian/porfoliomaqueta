f3e194589d9143388d9013a8a297cfd81d0307b6
Ruth  Sordián <99339056+ruthsordian@users.noreply.github.com>
2022-07-24 16:50:38 -0300

    Delete BDPorfolio.sql

--- BDPorfolio.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO persona
-VALUES (1,'Ruth','Sordián',
-'San Lorenzo 141 La Calera Departamento Colón Córdoba Argentina',
-'1977-05-25','3516885427','ruthsordian@gmail.com',' Soy Ruth Sordián y me interesa la tecnología y sus multiples facetas, lo que me llevó desde la tecnología en el área de la salud a la informática y las miles de soluciones que se pueden brindar desde todas las áreas de la programación.','images/fotoPerfil.jpeg.jpg')
